a0224b9b6625ae043c0c8ce44ce1087db7265f6a
Alessandro Federici <afederici75@users.noreply.github.com>
2024-07-04 13:47:29 -0400

    work

--- src/IntelliBlog.Domain/Blogs/BlogArticle.cs
+++ src/IntelliBlog.Domain/Blogs/BlogArticle.cs
@@ -2,20 +2,14 @@
 
 public class BlogArticle : HasDomainEventsBase
 {
-    public static BlogArticle CreateNew(BlogId blogId, ArticleId articleId, int sequence)
+    public static BlogArticle CreateNew(BlogId blogId, ArticleId articleId)
     {
         var blogArticle = new BlogArticle();
         blogArticle.BlogId = blogId;
         blogArticle.ArticleId = articleId;
-        blogArticle.Seq = sequence;
         return blogArticle;
     }
 
     public BlogId BlogId { get; private set; } = default!;
-    public ArticleId ArticleId { get; private set; } = default!;
-    public int Seq { get; private set; }
-    public void IncrementSequence()
-    {
-        Seq++;
-    }
+    public ArticleId ArticleId { get; private set; } = default!;    
 }
