d09b3ca43d7a83c094749cc15e0be88a4386df3f
Alessandro Federici <afederici75@users.noreply.github.com>
2024-07-07 09:06:43 -0400

    - Improved DeleteBlogCommandHandler and related -

--- src/IntelliBlog.Application/UseCases/Blogs/Delete/DeleteBlogCommandHandler.cs
+++ src/IntelliBlog.Application/UseCases/Blogs/Delete/DeleteBlogCommandHandler.cs
@@ -4,25 +4,23 @@ using IntelliBlog.Domain.Aggregates.Blogs;
 namespace IntelliBlog.Application.UseCases.Blogs.Delete;
 
 public class DeleteBlogCommandHandler(
-    IMediator _mediator,
-    IUnitOfWork _unitOfWork)
+    IPublisher _publisher,
+    IRepository<Blog> _repository)
     : ICommandHandler<DeleteBlogCommand, Result>
 {
     public async Task<Result> Handle(DeleteBlogCommand command, CancellationToken cancellationToken)
     {
-        var blogRepo = _unitOfWork.GetRepository<Blog>();
-        var blog = await blogRepo.GetByIdAsync(command.Id, cancellationToken);
+        var blog = await _repository.GetByIdAsync(command.Id, cancellationToken);
 
         if (blog == null)
         {
             return Result.NotFound("Blog not found");
         }
 
-        await _unitOfWork.GetRepository<Blog>().DeleteAsync(blog, cancellationToken);
+        await _repository.DeleteAsync(blog, cancellationToken);
 
-        await _unitOfWork.CompleteAsync(cancellationToken);
-
-        await _mediator.Publish(new BlogDeletedEvent(blog.Id));
+        // Events
+        await _publisher.Publish(new BlogDeletedEvent(blog.Id));
 
         return Result.Success();
     }
