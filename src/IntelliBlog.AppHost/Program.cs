51909a8087b5d1e91aa93803ce3949ae65f12f88
Alessandro Federici <afederici75@users.noreply.github.com>
2024-07-10 10:01:46 -0400

    Added test redis

--- src/IntelliBlog.AppHost/Program.cs
+++ src/IntelliBlog.AppHost/Program.cs
@@ -1,13 +1,19 @@
 ﻿var builder = DistributedApplication.CreateBuilder(args);
 
-//var dbPassword = builder.AddParameter("blogging-db-password", secret: true);
-
+// SQL Server
 var pwd = builder.AddParameter("blogging-db-password", secret: true);
-var sql = builder.AddSqlServer("sql", pwd, 12000)
+
+var sql = builder.AddSqlServer("sql", pwd, port: 12000)
                  .WithDataVolume("sql-data");
+
 var sqldb = sql.AddDatabase("IntelliBlogDb");
 
+// REDIS
+var cache = builder.AddRedis("cache", port: 12001);
+
+// References
 builder.AddProject<Projects.API>("api")
-    .WithReference(sqldb);
+       .WithReference(cache)
+       .WithReference(sqldb);
 
 builder.Build().Run();
