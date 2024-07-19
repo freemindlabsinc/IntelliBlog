14db54aa303d7b68341481df2ea426035ece45b1
Alessandro Federici <afederici75@users.noreply.github.com>
2024-07-19 19:03:46 -0400

    work

--- src/Domain/Aggregates/Articles/ArticleComment.cs
+++ src/Domain/Aggregates/Articles/ArticleComment.cs
@@ -1,12 +1,11 @@
 ﻿namespace Blogging.Domain.Aggregates.Articles;
 
-public sealed class ArticleComment : Entity<int>
+public sealed class ArticleComment : CommentBase
 {
     internal static ArticleComment CreateNew(        
         int articleId,
         string text,
         string commentedBy)
-        //,CommentId parentId = default)
     {
         var comment = new ArticleComment();
         comment.ArticleId = articleId; // Once-setter        
@@ -16,22 +15,7 @@ public sealed class ArticleComment : Entity<int>
         return comment;
     }
 
-    public int ArticleId { get; private set; } = default!;
-    public string Text { get; private set; } = default!;
-    public string CommentedBy { get; private set; } = default!;
-    // public CommentId? ParentId { get; private set; }
-
-    public void UpdateText(string text)
-    {
-        Guard.Against.NullOrWhiteSpace(text, nameof(text));
-        Text = text;     
-    }
-
-    public void UpdateCommentedBy(string commentedBy)
-    {
-        Guard.Against.NullOrWhiteSpace(commentedBy, nameof(commentedBy));
-        CommentedBy = commentedBy;
-    }
+    public int ArticleId { get; private set; } = default!;   
 
     // For Entity Framework
     private ArticleComment() { }
