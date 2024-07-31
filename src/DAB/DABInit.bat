6b17bc42aaef9f7dd2ec226f74784977559482d9
Alessandro Federici <afederici75@users.noreply.github.com>
2024-07-31 19:26:44 -0400

    Cleanup

--- src/DAB/DABInit.bat
+++
@@ -1,5 +0,0 @@
-dab init --database-type "mssql" --host-mode "Development" --connection-string "Server=localhost,1433;User Id=sa;Database=IntelliBlog_Dev;Password=G7i5z5mo;TrustServerCertificate=True;Encrypt=True;"
-
-dab add Blogs --source "dbo.Blogs" --permissions "anonymous:*"
-dab add Articles --source "dbo.Articles" --permissions "anonymous:*"
-dab add Sources --source "dbo.Sources" --permissions "anonymous:*"
